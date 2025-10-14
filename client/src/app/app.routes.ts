39b90f6a84ca31c0e0534dde502dc6df59dd7e91
Fwang0222 <1441777902@qq.com>
2025-10-14 14:57:09 +0800

    update  angular routes and style

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import {Details} from './details/details';
+import {Search} from './search/search';
+import {Home} from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home
+  },
+  {
+    path: 'search',
+    component: Search
+  },
+  {
+    path: 'details/:id',
+    component: Details
+  }
+];
--- db-details.js
+++ db-details.js
@@ -1,7 +1,7 @@
 module.exports = {
   host: 'localhost',
   user: 'root',
-  password: '000513',
+  password: '123456',
   database: 'charityevents_db'
 };
 
