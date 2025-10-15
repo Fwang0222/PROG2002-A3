2c828b23949578b368105bbad8c942cc00e97fae
SuyaZhang32 <1797443090@qq.com>
2025-10-15 14:26:55 +0800

    create heart component

--- client/src/app/admin/admin.ts
+++ client/src/app/admin/admin.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import {Navigation} from '../navigation/navigation';
 import {RouterModule} from '@angular/router';
+import {Heart} from '../heart/heart';
 
 type Tab = 'categories' | 'events' | 'registrations';
 
 @Component({
   selector: 'app-admin',
-  imports: [Navigation, RouterModule],
+  imports: [Navigation, RouterModule, Heart],
   templateUrl: './admin.html',
   styleUrl: './admin.css'
 })
